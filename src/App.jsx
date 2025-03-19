4bd6cbf9e63550eb122dafe5afac804007358b18
Ndang11 <royaltyhopsin@gmail.com>
2025-03-19 14:35:35 +0100

    rounding up with last styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import PackagePage from "./pages/PackagePage";
@@ -6,27 +6,23 @@ import GalleryPage from "./pages/GalleryPage";
 import ContactPage from "./pages/ContactPage";
 import Header from "./component/Header";
 import Banner from "./component/Banner";
-import "./App.css";
 import Footer from "./component/Footer";
+import "./App.css";
 
 function App() {
-
-
   return (
-    <>
+    <BrowserRouter>
       <Banner />
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/package" element={<PackagePage />} />
-          <Route path="/gallery" element={<GalleryPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/package" element={<PackagePage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
